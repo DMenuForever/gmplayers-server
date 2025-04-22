bd68d718859a929a1200a73da4fee9dacecd3d0d
Davuks <skibidilol553@gmail.com>
2025-04-22 19:32:07 +0800

    maybe fix idk
    trying to fix 404 error

--- server.js
+++ server.js
@@ -1,43 +1,34 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+const express = require('express')
+const app = express()
+const port = 8080
 
-let players = {};
+app.use(express.json())
 
-app.use(express.json());
+const players = {}
 
 app.post('/sync', (req, res) => {
-    const { id, pos, ang, weapon, message, map } = req.body;
+    const { id, pos, ang, weapon, map, message } = req.body
 
-    if (id) {
-        players[id] = {
-            pos,
-            ang,
-            weapon,
-            message,
-            map
-        };
+    if (!id) return res.status(400).json({ error: 'No ID provided' })
+
+    players[id] = { pos, ang, weapon, map }
+
+    const others = {}
+    for (const [pid, pdata] of Object.entries(players)) {
+        if (pid !== id) others[pid] = pdata
     }
 
-    const otherPlayers = {};
-    for (const [pid, data] of Object.entries(players)) {
-        if (pid !== id) {
-            otherPlayers[pid] = data;
-        }
-    }
-
-    res.json({ type: 'sync', players: otherPlayers });
-});
+    res.json({ players: others })
+})
 
 app.get('/status', (req, res) => {
-    let statusHTML = '<h1>Онлайн Игроки</h1><ul>';
-    for (const [id, data] of Object.entries(players)) {
-        statusHTML += `<li>${id} - Карта: ${data.map}</li>`;
-    }
-    statusHTML += '</ul>';
-    res.send(statusHTML);
-});
+    const list = Object.entries(players).map(([id, data]) => ({
+        id,
+        map: data.map || 'unknown'
+    }))
+    res.json({ online: list })
+})
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+    console.log(`Server on ${port}`)
+})
